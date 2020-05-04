78dd1a77e5f6d89f9fcf152e7a9978e8555044a8
sweyome <sweyome1@gmail.com>
2020-05-03 21:07:55 -0400

    project update

--- adv.py
+++ adv.py
@@ -75,7 +75,89 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+counter = 0
+score = 2000
+lowest_score = 2000
 
+while score > 959:
+    player = Player(world.starting_room)
+
+    # Fill this out with directions to walk
+    # traversal_path = ['n', 'n']
+    traversal_path = []
+    graph = {}
+    num_rooms = 500
+    opposite = {'s': 'n', 'n': 's', 'w': 'e', 'e': 'w'}
+
+    '''
+    Pick a random unexplored direction
+    Travel that direction and log the step in your route
+    Then loop
+    '''
+    source = None 
+    old_room = None
+    no_back = True
+    visited = set()
+
+    while len(visited) < num_rooms:  
+        # get a list of exits from the current room
+        current_room = player.current_room.id
+        # print(f"You are in Room {current_room}")
+        visited.add(current_room)
+
+        exits = player.current_room.get_exits() # returns list of exits
+        # print(f"The exits are {exits}")
+
+        if current_room not in graph:
+            # print(f"Room {current_room} not in graph.")
+            graph[current_room] =  {}
+            # print("Added!")
+            # print(f"Graph length is {len(graph)}")
+            for e in exits:
+                graph[current_room][e]= '?'
+        
+        if len(visited) == num_rooms:
+            # print(f"Break. Length of traversal is {len(traversal_path)}")
+            counter += 1
+            if counter%1000==0:
+                print(f"Attempt #{counter}. Lowest score is {lowest_score}.")
+            break
+
+        # print(f"length of graph: {len(graph)}")
+        if source and no_back:
+            graph[current_room][source] = old_room
+        no_back = True
+        
+        # print(graph[current_room])
+
+        unexplored_exits_list = unexplored_exits(graph, current_room)
+        # print(f"Unexplored exits: {unexplored_exits_list}")
+
+        
+        # record the connections
+        # randomly select an exit and travel in that direction
+        if len(unexplored_exits_list)>0:
+            direction = random.choice(unexplored_exits_list)
+            player.travel(direction)
+            graph[current_room][direction] = player.current_room.id
+            source = opposite[direction]
+            old_room = current_room
+            
+
+            traversal_path.append(direction)
+            # print(f"trav_path: {traversal_path}")
+            # print(f"Traversal path: {traversal_path}") 
+        else:
+            # print("Backtracking...")
+            backtrack_path_list = backtrack_path(graph, player)
+            # print(f"Backtrack list: {backtrack_path_list}")
+            for d in backtrack_path_list:
+                player.travel(d)
+                traversal_path.append(d)
+            no_back = False
+    score = len(traversal_path)
+    if score < lowest_score:
+        lowest_score = score
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
